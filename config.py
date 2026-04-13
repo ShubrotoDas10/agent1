1d420489f9d77449d1f44520a3d5e2ecbe754b68
ShubrotoDas10 <sd.shubroto10@gmail.com>
2026-04-13 04:56:55 +0000

    fix database url to use pooler

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "12345678")
 DB_HOST     = os.getenv("DB_HOST", "localhost")
 DB_PORT     = os.getenv("DB_PORT", "5432")
 DB_NAME     = os.getenv("DB_NAME", "searcher")
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres.hqitqjhmmcuqlofrqhnf:Shubroto123@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres")
 
 # Server
 API_HOST = "0.0.0.0"
